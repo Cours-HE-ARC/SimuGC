de0145bf24c61b07b7570b9a3086269218603d51
SCE <SCE@host.docker.internal>
2019-08-16 12:22:48 +0200

    add parameters config

--- src/ch/jee/gc/Main.java
+++ src/ch/jee/gc/Main.java
@@ -11,10 +11,16 @@ public class Main {
 
 	static final List<TestObject> objects = new ArrayList<>();
 	private static final int UN_MILLION = 1000000;
-	private static final long MAX_TIME_EXECUTION = 1000 * 30; //30s
+	private static long MAX_TIME_EXECUTION = 1000 * 120;
+	private static boolean CLEAR_MODE = Boolean.TRUE;
+	private final static String NO_CLEAR_MODE_ARGS_VALUE = "noclear";
 	
 	
 	public static void main(String[] args) throws InterruptedException {
+		
+		System.out.println("Starting object generation, wait 5 seconds before running");
+		
+		checkAndApplyProgrammArguments(args);
 		
 		Thread.sleep(5000);
 		
@@ -24,35 +30,50 @@ public class Main {
 	
 	static void start() {
 		
+		System.out.println("Starting object generation...");
+		
 		Date startDate = new Date();
 		
 		while(true) {
 			TestObject to = new TestObject(1, 1);
 			objects.add(to);
-			if(objects.size() == UN_MILLION) {
+			
+			if(CLEAR_MODE && objects.size() == UN_MILLION) {
 				objects.clear();
 			}
 		
 			checkTimeToKill(startDate);
 		}
+		
+		
 	}
 
 	private static void checkTimeToKill(Date startDate) {
 		if(new Date().getTime() - startDate.getTime() > MAX_TIME_EXECUTION) {
+			System.out.println("Ending object generation. Programm will exit now");
 			System.exit(0);
 		}
 	}
 
 	
+	private static void checkAndApplyProgrammArguments(String arguments[]) {
+		
+		if(arguments.length == 2) {
+			String clearMode = arguments[0];
+			Integer duration = Integer.parseInt(arguments[1]);
+			
+			System.out.println("Program argument found, clearMode: "  +clearMode);
+			System.out.println("Program argument found, duration: "  + duration + " seconds");
+			
+			MAX_TIME_EXECUTION = duration * 1000;
+			
+			CLEAR_MODE = (clearMode.equals(NO_CLEAR_MODE_ARGS_VALUE)) ? Boolean.FALSE : Boolean.TRUE;
+			
+		}else {
+			System.out.println("No program arguments found, or not property configured");
+		}
+	}
+	
 	
 }
 
-class TestObject {
-	private int a;
-	private int b;
-	
-	public TestObject(int a, int b){
-		this.a = a;
-		this.b = b;
-	}
-}
---
+++ src/ch/jee/gc/TestObject.java
@@ -0,0 +1,13 @@
+package ch.jee.gc;
+
+public class TestObject {
+	private int a;
+	private int b;
+	
+	public TestObject(int a, int b){
+		this.a = a;
+		this.b = b;
+	}
+
+	
+}
